d4a91b3f25e2db047b13b646f9479500626dd668
Felipe Zambrano <felipe.zambrano@gmail.com>
2024-06-24 19:31:40 -0400

    Ejercicio lógica booleana

---
+++ 02 Lógica booleana/op1.py
@@ -0,0 +1,7 @@
+object_size = int(input('Ingresa el tamaño (m3) del objeto: '))
+proximity = int(input('Ingresa la proximidad (km): '))
+
+if object_size > 5 and proximity < 1000:
+    print('Se requieren maniobras evasivas')
+else:
+    print('El objeto no representa amenaza')
