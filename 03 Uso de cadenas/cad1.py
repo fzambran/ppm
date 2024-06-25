e98790489dd4309862bc6a0a28605ee93347da7a
Felipe Zambrano <felipe.zambrano@gmail.com>
2024-06-24 21:47:08 -0400

    Ejemplos con cadenas

---
+++ 03 Uso de cadenas/cad1.py
@@ -0,0 +1,22 @@
+fact = "The  Moon has no atmosphere."
+print(fact)
+
+# Nueva línea
+print("\n")
+
+text = """We only see about 60% of the Moon's surface, this is known as the "near side"."""
+print(text)
+
+print("\n")
+
+#Texto multilínea
+multiline = "Facts about the Moon:\n There is no atmosphere.\n There is no sound."
+print(multiline)
+
+print("\n")
+
+# Otra forma de hacer lo anterior
+multiline = """Facts about the Moon:
+ There is no atmosphere. 
+ There is no sound."""
+print(multiline)
